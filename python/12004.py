5a262b49f93c9c70f111d4cb8eab4ff0550f53ec
www-jong <weon1009@gmail.com>
2022-12-26 17:44:23 +0900

    1226

--- python/12004.py
+++ python/12004.py
@@ -4,5 +4,4 @@ for i in range(2,1000000):
     if tmp[i]==1:
         res.append(i)
         tmp[i::i]=[0]*(1000000//i)
-print(res)
-print(res[-1])
+print(*res)
--- python/3260.py
+++ python/3260.py
@@ -1,6 +1,6 @@
 res=[]
 for m in range(int(input())):
-    tmp=""
+    tmp=sum(list(map(int,input().split())))
     res.append(tmp)
 for i in range(len(res)):
     print("#%d %s"%(i+1,res[i]))
