591e6e2dc37731d5ae773a793d5a6118f81c90be
Wjong <weon1009@gmail.com>
2022-12-30 00:54:08 +0900

    1230

---
+++ python/3809.py
@@ -0,0 +1,27 @@
+ans=[]
+for m in range(int(input())):
+    tmp=0
+    N=int(input())
+    li=[]
+    lilen=0
+    while lilen!=N:
+        tmpli=list(map(int,input().split()))
+        lilen+=len(tmpli)
+        li+=tmpli
+    co=[0]*10
+    for i in li:
+        co[i]+=1
+    c=0
+    while tmp==0:
+        tmpco=[0]*10
+        for i in str(c):
+            tmpco[int(i)]+=1
+        for i in range(10):
+            if co[i]<tmpco[i]:
+                tmp=c
+                break
+        c+=1
+    print(tmp)
+    ans.append(tmp)
+for i in range(len(ans)):
+    print("#%d %s"%(i+1,ans[i]))
--- python/test.py
+++ python/test.py
@@ -1,3 +1,2 @@
-a=[1,2,3,7,8]
-a.insert(2,100)
-print(a)
+import antigravity
+antigravity
