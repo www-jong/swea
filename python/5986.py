0124b917c8f430fce89b939b8537f9f18f862bd4
www-jong <weon1009@gmail.com>
2023-01-04 20:45:01 +0900

    0104

--- python/5986.py
+++ python/5986.py
@@ -11,10 +11,26 @@ for i in range(2,MAXNUM):
         sodic[i]=1
 for m in range(int(input())):
     tmp=0
+    minus_tmp=0
+    all_compare_tmp=0
     N=int(input())
-    if N/3%1==0 and sodic[N//3]:
-        tmp+=1
-    
-    res.append(tmp)
+    dic={}
+    for i in so:
+        for j in so:
+            for z in so:
+                if (i+j+z)>N:
+                    break
+                elif (i+j+z)==N:
+                    checker=0
+                    for o in (str(i)+str(j)+str(z),str(i)+str(z)+str(j),str(j)+str(i)+str(z),str(j)+str(z)+str(i),str(z)+str(i)+str(j),str(z)+str(j)+str(i)):
+                        if o not in dic:
+                            dic[o]=1
+                            checker=1
+                    if checker==1:
+                        print(f'i:{i}, j:{j}, z:{z}')
+                        tmp+=1
+                    
+    print(f'tmp : {tmp} minus_tmp : {minus_tmp} all,, {all_compare_tmp}')
+    res.append(tmp-minus_tmp)
 for i in range(len(res)):
     print("#%d %s"%(i+1,res[i]))
