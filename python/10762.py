de673b21f103cff32fbdafdf7410a4d83d83cb1a
Wjong <weon1009@gmail.com>
2023-02-18 19:13:26 +0900

    0218

---
+++ python/10762.py
@@ -0,0 +1,9 @@
+res=[]
+for m in range(int(input())):
+    tmp=""
+    N=int(input())
+    li=list(map(int,input().split()))
+    
+    res.append(tmp)
+for i in range(len(res)):
+    print("#%d %s"%(i+1,res[i]))
